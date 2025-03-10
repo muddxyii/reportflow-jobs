4f2aaf7a57c7460a45feab2263cac5f5523b1cd8
muddxyii <muddxyii@gmail.com>
2025-03-10 14:51:05 -0700

    feat: Add icons to action buttons on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import {Car, FileCog, FilePlus2} from "lucide-react";
+import React from "react";
+
 export default function Home() {
     return (
         <main className="min-h-screen bg-base-300 flex flex-col items-center justify-center space-y-6">
@@ -11,18 +14,21 @@ export default function Home() {
                             className="btn btn-primary w-full"
                             onClick={() => window.location.href = '/new'}
                         >
+                            <FilePlus2 className="w-4 h-4 mr-2"/>
                             Create New RF Job
                         </button>
                         <button
                             className="btn btn-secondary w-full"
                             onClick={() => window.location.href = '/edit'}
                         >
+                            <FileCog className="w-4 h-4 mr-2"/>
                             Edit Existing RF Job
                         </button>
                         <button
                             className="btn btn-warning w-full"
                             onClick={() => window.location.href = '/job-router'}
                         >
+                            <Car className="w-4 h-4 mr-2"/>
                             Route RF Jobs
                         </button>
                     </div>
